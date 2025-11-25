2e8af0977dcf50602ecc5440aa9ad8a7750e5ca0
kumaresang12 <73640429+kumaresang12@users.noreply.github.com>
2025-11-24 21:21:57 -0800

    Change default instance type and clean up variables
    Updated the default instance type to 't3.micro' and removed unused variables.

--- variables.tf
+++ variables.tf
@@ -7,23 +7,11 @@ variable "aws_region" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.small"
+  default     = "t3.micro"
 }
 
 variable "key_pair_name" {
   description = "Name of the existing EC2 Key Pair"
   type        = string
   default     = "my-key-pair"
-}
-
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = list(string)
-  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
