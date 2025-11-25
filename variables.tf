1af48f646191cfb9dce0aa339a8b10ba03857f4b
kumaresang12 <73640429+kumaresang12@users.noreply.github.com>
2025-11-24 19:45:20 -0800

    Add Terraform variables for AWS configuration

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "aws_region" {
+  description = "AWS region to create resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.small"
+}
+
+variable "key_pair_name" {
+  description = "Name of the existing EC2 Key Pair"
+  type        = string
+  default     = "my-key-pair"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
